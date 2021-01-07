1c4e6f2cc9c59c5a3870f1d012cd512645b540c3
kaworu <kaworu.ochi@gmail.com>
2021-01-07 23:36:42 +0900

    simple Declarative Renderting example

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ const HelloWorld = {
     }
   }
   
-  Vue.createApp(HelloWorld).mount('#helloworld')
+Vue.createApp(HelloWorld).mount('#helloworld')
+
+// Declarative Rendering (宣言的レンダリング)
+// declaratively render data to the DOM using straightforward template syntax:
+// The data and the DOM are now linked, and everything is now reactive.
+const CounterApp ={
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(()=>{
+      this.counter++
+    }, 1000)
+  }
+}
+Vue.createApp(CounterApp).mount('#counter')
